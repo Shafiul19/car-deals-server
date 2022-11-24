d6585dc4d3cc3c986eab011dfc91134d708cd52f
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-24 20:28:25 +0600

    admin varification api create

--- index.js
+++ index.js
@@ -54,10 +54,19 @@ async function run() {
         console.log(user);
         res.status(403).send({ accessToken: '' })
     })
+    // All user
     app.get('/users', verifyJWT, async (req, res) => {
         const query = {};
         const users = await usersCollection.find(query).toArray();
         res.send(users)
+    })
+    // Admin 
+
+    app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isAdmin: user?.role === 'admin' })
     })
 
     // save user to database
